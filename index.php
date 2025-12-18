68020699bef29a4548ea0115f0598432834293a6
Davi Rodrigues de Almeida <davirralmeida@gmail.com>
2025-12-18 14:36:46 -0300

    mudei o services pq tava muito fragmentado

--- index.php
+++ index.php
@@ -21,11 +21,15 @@ require_once "Framework.php";
 // Documentos::C_arquivos(caminho:"api/dao",arquivos:["Autores","Usuarios","Generos","Generos_Mangas","Rankings","Avaliacoes","Favoritos","Comentarios_Manga","Capitulos"]);
 // Documentos::C_arquivos(caminho:"api/controller",arquivos:["Autora"]);
 
-Documentos::C_pastas(caminho:"api/services",pastas:["autores","capitulos","generos_mangas","generos","mangas","usuarios"]);
-Documentos::C_arquivos(caminho:"api/services/autores",arquivos:["Post.php","Delete.php","Put.php","Get.php"]);
-Documentos::C_arquivos(caminho:"api/services/capitulos",arquivos:["Post.php","Delete.php","Put.php","Get.php"]);
-Documentos::C_arquivos(caminho:"api/services/generos_mangas",arquivos:["Post.php","Delete.php","Put.php","Get.php"]);
-Documentos::C_arquivos(caminho:"api/services/generos",arquivos:["Post.php","Delete.php","Put.php","Get.php"]);
-Documentos::C_arquivos(caminho:"api/services/mangas",arquivos:["Post.php","Delete.php","Put.php","Get.php"]);
-Documentos::C_arquivos(caminho:"api/services/usuarios",arquivos:["Post.php","Delete.php","Put.php","Get.php"]);
+Documentos::C_pastas(caminho:"api",pastas:["services"]);
+// Documentos::C_pastas(caminho:"api/services",pastas:["autoresService","capitulosService","generos_mangasService","generosService","mangasService","usuariosService"]);
+// Documentos::C_arquivos(caminho:"api/services/autoresService",arquivos:["PostService.php","DeleteService.php","PutService.php","GetService.php"]);
+// Documentos::C_arquivos(caminho:"api/services/capitulosService",arquivos:["PostService.php","DeleteService.php","PutService.php","GetService.php"]);
+// Documentos::C_arquivos(caminho:"api/services/generos_mangasService",arquivos:["PostService.php","DeleteService.php","PutService.php","GetService.php"]);
+// Documentos::C_arquivos(caminho:"api/services/generosService",arquivos:["PostService.php","DeleteService.php","PutService.php","GetService.php"]);
+// Documentos::C_arquivos(caminho:"api/services/mangasService",arquivos:["PostService.php","DeleteService.php","PutService.php","GetService.php"]);
+// Documentos::C_arquivos(caminho:"api/services/usuariosService",arquivos:["PostService.php","DeleteService.php","PutService.php","GetService.php"]);
+
+Documentos::C_arquivos(caminho:"api/services",arquivos:["class.AutoresService.php","class.CapitulosService.php","class.Generos_mangasService.php","class.GenerosService.php","class.MangasService.php","class.UsuariosService.php"]);
+
 ?>
